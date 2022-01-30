fe16a002066c3c64a861cc4ee23193163eee312a
Mime Čuvalo <mimecuvalo@gmail.com>
2022-01-30 09:33:31 -0800

    csp

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const CircularDependencyPlugin = require('circular-dependency-plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   webpack: (config, options) => {
     config.plugins.push(
       new CircularDependencyPlugin({
@@ -17,11 +18,65 @@ const nextConfig = {
         allowAsyncCycles: false,
         // set the current working directory for displaying module paths
         cwd: process.cwd(),
-      }),
+      })
     );
 
     return config;
   },
+
+  async headers() {
+    const isDevelopment = process.env.NODE_ENV === 'development';
+    const cspDirectives = {
+      'connect-src': [isDevelopment ? '*' : "'self'"],
+      'default-src': ["'none'"],
+      'font-src': ["'self'", 'https:'],
+      'frame-ancestors': ["'self'"],
+      'frame-src': ["'self'", 'http:', 'https:'],
+      'img-src': ['data:', 'http:', 'https:'],
+      'manifest-src': ["'self'"],
+      'media-src': ["'self'", 'blob:'],
+      'object-src': ["'self'"],
+      'report-uri': ['/api/report-csp-violation'],
+      'script-src': ["'self'", 'https://cdn.auth0.com', 'https://storage.googleapis.com'].concat(
+        isDevelopment ? ["'unsafe-inline'", "'unsafe-eval'"] : []
+      ),
+      'upgrade-insecure-requests': [],
+
+      // XXX(mime): we have inline styles around - can we pass nonce around the app properly?
+      'style-src': ["'self'", 'https:', "'unsafe-inline'"], //(req, res) => `'nonce-${res.locals.nonce}'`],
+    };
+    const cspValues = Object.keys(cspDirectives)
+      .map((directive) => `${directive} ${cspDirectives[directive].join(' ')}`)
+      .join('; ');
+
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer-when-downgrade',
+          },
+          {
+            key: isDevelopment ? 'Content-Security-Policy-Report-Only' : 'Content-Security-Policy',
+            value: cspValues,
+          },
+        ],
+      },
+    ];
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/api/csp-report-violation.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log('CSP Violation:', { data: req.body.data });
-  res.status(204).send('');
-}
