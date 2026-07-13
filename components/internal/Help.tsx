31679e0addf43e0f2f2575cb875d1f5568b0f6dc
Mime Čuvalo <mimecuvalo@gmail.com>
2026-07-13 22:56:32 +0100

    tweaks

--- components/internal/Help.tsx
+++ components/internal/Help.tsx
@@ -35,11 +35,13 @@ export default function Help() {
           fill="none"
           stroke="currentColor"
           strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
           aria-hidden="true"
         >
           <circle cx="12" cy="12" r="10" />
           <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-          <line x1="12" y1="17" x2="12.01" y2="17" />
+          <path d="M12 17h.01" />
         </svg>
       </Menu.Trigger>
       <Menu.Portal>
